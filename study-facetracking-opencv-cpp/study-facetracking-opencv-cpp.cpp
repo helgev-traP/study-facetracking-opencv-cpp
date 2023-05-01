50688992af50cb128b54917a99c341787a0da923
helgev-traP <113248435+helgev-traP@users.noreply.github.com>
2023-05-02 00:01:26 +0900

    構造体を3つ作った
    head_position poler_coordes(face_coordinates face, camera_info info)を書く

--- study-facetracking-opencv-cpp/study-facetracking-opencv-cpp.cpp
+++ study-facetracking-opencv-cpp/study-facetracking-opencv-cpp.cpp
@@ -12,25 +12,33 @@ using std::endl;
 
 #define ll long long
 
-class face_placeSize
+// --- struct
+
+struct face_coordinates
 {
-public:
-    class centor
+    // struct
+    struct center_of_face
     {
         int x;
         int y;
     };
-    class size
+    struct size_of_face
     {
         int width;
         int height;
+    };
+    // val
+    center_of_face center;
+    size_of_face size;
+    double face_size()
+    {
+        return (size.width + size.height) / 2.0;
     }
 };
 
-class camera_info
+struct camera_info
 {
-public:
-    class camera
+    struct camera
     {
         double width;
         double height;
@@ -40,9 +48,18 @@ public:
     double face_size;
 };
 
-std::vector<double>
-poler_coordes(std::vector<int> centor, std::vector<int> size, camera_info data)
-// 入力: 顔の中心の画像上の座標, サイズ, ,
+struct head_position
+{
+    double horizontal_angle;
+    double vertical_angle;
+    double distance;
+};
+
+
+// --- function
+
+head_position poler_coordes(face_coordinates face, camera_info info)
+// 入力: face_coordinates, camera_info
 // 出力: (正面を0とし、カメラから右側・上側を正として) 横の角度, 縦の角度, 距離
 {
 }
@@ -61,22 +78,36 @@ int main()
     cv::CascadeClassifier cascade;
     cascade.load("../haarcascades/haarcascade_eye_tree_eyeglasses.xml");
 
+    // カメラの情報と基本の位置での顔の大きさを記録しておく
+    camera_info cam_info;
+
+    // 無限ループ
     while (1)
     {
         // 映像をとってくる
         cap >> frame;
+
         // カスケード
         std::vector<cv::Rect> faces;
         cascade.detectMultiScale(frame, faces, 1.1, 3, 0, cv::Size(20, 20));
+
         // カスケードから矩形描画
         for (int i = 0; i < faces.size(); i++)
         {
             cv::rectangle(frame, cv::Point(faces[i].x, faces[i].y), cv::Point(faces[i].x + faces[i].width, faces[i].y + faces[i].height), cv::Scalar(0, 0, 255), 3);
         }
+
         // faces[0]に対してカメラからの相対座標を計算する
-        std::vector<double> coordes = poler_coordes({faces[0].x + faces[0].width / 2, faces[0].y + faces[0].height}, {faces[0].width, faces[0].height});
+        face_coordinates face_coords;
+        face_coords.center.x = faces[0].x + faces[0].width / 2;
+        face_coords.center.y = faces[0].y + faces[0].height / 2;
+        face_coords.size.width = faces[0].width;
+        face_coords.size.height = faces[0].height;
+        head_position head_pos = poler_coordes(face_coords, cam_info);
+
         // 描画
         cv::imshow("win", frame);
+
         // break
         const int key = cv::waitKey(1);
         if (key == 'q' /*113*/)
